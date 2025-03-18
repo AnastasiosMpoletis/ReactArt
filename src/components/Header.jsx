3bb94ae74f7da8f4e7d00d77145fcd11ea33d4ba
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-03-18 20:46:09 +0200

    L-116. Create Header, Button Styled Components, integrating nested rules, pseudo-selectors and media queries.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,56 @@
+import { styled } from 'styled-components';
+
 import logo from '../assets/logo.png';
-import classes from './Header.module.css';
+// import classes from './Header.module.css';
+
+/**
+ * We use the '&' character to point to out parent attribute (in this case the header).
+ * In this case -> @media, we can remove the header assign for margin-bottom.
+ */
+const StyledHeader = styled.header`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 2rem;
+    margin-bottom: 2rem;        
+    
+    & img {
+      object-fit: contain;
+      margin-bottom: 2rem;
+      width: 11rem;
+      height: 11rem;
+    }
+    
+    & h1 {
+      font-size: 1.5rem;
+      font-weight: 600;
+      letter-spacing: 0.4em;
+      text-align: center;
+      text-transform: uppercase;
+      color: #9a3412;
+      font-family: 'Pacifico', cursive;
+      margin: 0;
+    }
+    
+    & p {
+      text-align: center;
+      color: #a39191;
+      margin: 0;
+    }
+    
+    @media (min-width: 768px) {
+      margin-bottom: 4rem;
+    
+      & h1 {
+        font-size: 2.25rem;
+      }
+    }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       {/* Inline styles */}
@@ -12,7 +59,7 @@ export default function Header() {
         textAlign: "left" // or 'text-align': "left"
       }}>A community of artists and art-lovers. */}
       {/* <p className={true ? classes.paragraph : undefined}>A community of artists and art-lovers.</p> */}
-      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
-    </header>
+      <p>A community of artists and art-lovers.</p>
+    </StyledHeader>
   );
 }
