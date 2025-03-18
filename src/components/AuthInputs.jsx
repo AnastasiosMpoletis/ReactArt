7f821649feab220040680661467acec633e6a732
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-03-18 21:05:51 +0200

    L-117. Refactor Button, Label, Input Styled Components to a different jsx file. Create reusable CustomInput Styled Component that wrapps Label and Input

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
 
+import Button from './Button.jsx';
+// We can have a different name here (instead of CustomInput)
+import Input from './Input.jsx';
+
 /**
  * Tagged template. We use standard CSS code here. No camel-case.
  * Creates a <div> and passes all props and children.
@@ -10,42 +14,6 @@ const ControlContainer = styled.div`
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`;
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
-  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
-  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
 `;
 
 export default function AuthInputs() {
@@ -71,30 +39,28 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p className='paragraph'>
-          {/* Use '$' to avoid clash with default build-in props that might exist on elements. */}
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            type="email"
-            // style={{
-            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
-            // }}
-            // In cases like this, we should use the full ? expression
-            // className={emailNotValid ? 'invalid' : undefined}
-            $invalid={emailNotValid}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
-            type="password"
-            $invalid={passwordNotValid}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
+        {/* Use '$' to avoid clash with default build-in props that might exist on elements. */}
+        <Input
+          label="Email"
+          invalid={emailNotValid}
+          type="email"
+          // style={{
+          //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+          // }}
+          // In cases like this, we should use the full ? expression
+          // className={emailNotValid ? 'invalid' : undefined}
+          // $invalid={emailNotValid}
+          onChange={(event) => handleInputChange('email', event.target.value)}
+        />
+        <Input
+          label="Password"
+          invalid={passwordNotValid}
+          type="password"
+          // $invalid={passwordNotValid}
+          onChange={(event) =>
+            handleInputChange('password', event.target.value)
+          }
+        />
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
