a6501466073792f427101014de185002eba86769
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-03-19 23:18:47 +0200

    L-121. Refactor Button to Tailwind and add hover. Add responsiveness to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,22 @@ import logo from '../assets/logo.png';
 export default function Header() {
   return (
     <header className="
-      flex flex-col 
+      flex 
+      flex-col 
       items-center 
-      mt-8 mb-16"
-    >
+      mt-8 
+      mb-8
+      md:mb-16
+    ">
       <img src={logo} alt="A canvas" className="
         mb-8 
         w-44 
         h-44 
-        object-contain"
-      />
+        object-contain
+      "/>
       <h1 className='
-        text-4xl 
+        text-xl
+        md:text-4xl 
         font-semibold 
         tracking-widest 
         text-center 
