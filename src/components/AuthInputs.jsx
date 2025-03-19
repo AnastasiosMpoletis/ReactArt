49d25857ad6d9ecbb51f42ff64c6b2df2d075d40
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-03-20 00:03:42 +0200

    L-122, 123. Refactor CustomInput to Input by adding conditional Tailwind css. Refactor AuthInputs to Tailwind css

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,20 +1,9 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 
 import Button from './Button.jsx';
 // We can have a different name here (instead of CustomInput)
 import Input from './Input.jsx';
 
-/**
- * Tagged template. We use standard CSS code here. No camel-case.
- * Creates a <div> and passes all props and children.
- */
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -37,8 +26,23 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className="
+      w-full 
+      max-w-sm 
+      p-8 
+      mx-auto
+      rounded 
+      shadow-md
+      bg-gradient-to-b
+      from-stone-700
+      to-stone-800
+    ">
+      <div className="
+        flex 
+        flex-col 
+        gap-2 
+        mb-6
+      ">
         {/* Use '$' to avoid clash with default build-in props that might exist on elements. */}
         <Input
           label="Email"
@@ -61,9 +65,9 @@ export default function AuthInputs() {
             handleInputChange('password', event.target.value)
           }
         />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
